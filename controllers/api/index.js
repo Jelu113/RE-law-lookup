92e8fdead53abe362c10da24b252c6be145046c1
JonBay <134355923+JonBay@users.noreply.github.com>
2023-09-28 22:07:49 -0600

    progress on login

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const keywordRoutes = require('./keywordRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/keywords', keywordRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
-//const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -17,17 +17,17 @@ const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
-  // cookie: {
-  //   maxAge: 300000,
-  //   httpOnly: true,
-  //   secure: false,
-  //   sameSite: 'strict',
-  // },
+   cookie: {
+     maxAge: 300000,
+     httpOnly: true,
+     secure: false,
+     sameSite: 'strict',
+   },
   resave: false,
   saveUninitialized: true,
-  // store: new SequelizeStore({
-  //   db: sequelize
-  // })
+   store: new SequelizeStore({
+     db: sequelize
+   })
 };
 
 

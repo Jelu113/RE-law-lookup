09d525fb3d073ec633f9258c14d592d84b492bf5
Nathan Greeff <nathangreeff@hotmail.com>
2023-10-01 17:53:13 -0600

    sorting keywords, visualy enhanced keywords

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,12 +1,16 @@
 //Handlebars helpers
 module.exports = {
 //Limited Each loop
-limitedEach(context, iterations, options) {
-    var ret = '';
-    for (var i = 0, j = Math.min(context.length, iterations); i < j; i++) {
-        ret = ret + options.fn(context[i]);
-    }
-    return ret;
+    limitedEach(context, iterations, options) {
+        var ret = '';
+        for (var i = 0, j = Math.min(context.length, iterations); i < j; i++) {
+            ret = ret + options.fn(context[i]);
+        }
+        return ret;
+    },
+
+    sortedKeywords(keywords) {
+        return keywords.sort((a,b) => a.keyword.localeCompare(b.keyword));
     }
 
 }
